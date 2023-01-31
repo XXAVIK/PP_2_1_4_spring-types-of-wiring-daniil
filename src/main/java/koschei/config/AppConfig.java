541f4e244a0d52943fad4e85e875dce6a98796bd
XXAVIK <ddg1377@gmail.com>
2023-01-31 19:44:36 +0300

    -1st cm

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,7 +1,7 @@
 package koschei.config;
 
-import koschei.models.Island2;
-import koschei.models.Wood3;
+import koschei.KoscheiTheDeathless;
+import koschei.models.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +10,4 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "koschei")
 public class AppConfig {
 
-    @Bean
-    public static Island2 getIsland(Wood3 wood) {
-        return new Island2(wood);
-    }
 }
